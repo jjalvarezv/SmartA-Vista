15abadd35da1e2d160a6e45ec00ba12028af981a
Juan Jose Alvarez <jjalvarezv97@gmail.com>
2020-06-24 00:41:41 -0500

    Agrego rutas al app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//importo componentes
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{path : '' , component : HomeComponent},
+	{path : '/home' , component : HomeComponent},
+	{path : '/about-us' , component : AboutUsComponent},
+	{path : '/contacto' , component : ContactoComponent},
+	{path : '/registro' , component : RegistroComponent},
+	{path : '**' , component : HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SmartA-Vista';
+    title = 'SmartA-Vista';
+
+    
+
 }
+
