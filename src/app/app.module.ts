4daef3b318e26c4482f95586cefab874c9481380
Juan Jose Alvarez <jjalvarezv97@gmail.com>
2020-06-24 00:14:24 -0500

    Creacion de componentes necesarios
    Se crean componentes home, registro, about-us y contacto, ademas se importan las img necesarias en assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    ContactoComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
